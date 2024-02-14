47c151d65b928a8b57d41a0b4c7c10c33587b295
Andrzej Kawula <andrzej.kawula@gmail.com>
2024-02-14 10:35:16 +0100

    feat: place for measuring dora metrics

--- src/Jira.tsx
+++ src/Jira.tsx
@@ -37,6 +37,12 @@ export default function Jira() {
                 }}
             >
                 <Typography level="h2">Jira</Typography>
+                <Typography>
+                    Deployment Frequency — How often an organization successfully releases to production <br />
+                    Lead Time for Changes — The amount of time it takes a commit to get into production <br />
+                    Change Failure Rate — The percentage of deployments causing a failure in production <br />
+                    Time to Restore Service — How long it takes an organization to recover from a failure in production <br />
+                </Typography>
                 <Loading />
             </Box>
         </Box>
